efc46a3689de1f31fd589578e45f0004575cdafe
SivaPachu <pachupy512@gmail.com>
2026-08-28 19:56:34 +0530

    Fix React accessibility warnings

--- src/components/PerfumeList.js
+++ src/components/PerfumeList.js
@@ -4,24 +4,27 @@ import three from "../assets/images/three.jpg"
 
 // Perfume List Component
 function PerfumeList() {
-  return(
-    <div class="products">
+    return (
+        <div className="products">
             <div className="box">
-                        <img src={one} alt="image"></img>
-                        <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>    
+                <img src={one} alt="Fogg Wood Extreme Perfume" />
+                <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>
             </div>
-            
+
             <div className="box">
-                <img src={two} alt="villain"></img>
-                <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p> 
+                <img src={two} alt="Villain Perfume" />
+                <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>
             </div>
+
             <div className="box">
-                <img src={three} alt="Designs Club"></img>
-                <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>
+                <img src={three} alt="Designer Club Be Spoke Perfume" />
+                <p>
+                    Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long
+                    Lasting Perfume
+                </p>
             </div>
-            
         </div>
-  )
+    );
 }
 
 export default PerfumeList
